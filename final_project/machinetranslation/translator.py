38e892460a3d66ae09a712ef55442d4866ec82a9
Amit Sinha <amitks0705@gmail.com>
2023-03-31 04:22:45 -0400

    translate
    Signed-off-by: Amit Sinha <amitks0705@gmail.com>

---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,5 @@
+def translateEnglishToFrench(textToTranslate):
+    return "Translated text to French: " + textToTranslate
+
+def translateFrenchToEnglish(textToTranslate):
+    return "Translated text to English: " + textToTranslate
